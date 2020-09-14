16a3d1fd02542eb73554ac2bcad9fb46f4cd8fec
Jasper <1049578026@qq.com>
2020-09-14 19:27:44 +0800

    modify: add comment

--- unittest/calc_test.go
+++ unittest/calc_test.go
@@ -2,6 +2,10 @@ package main
 
 import "testing"
 
+/**
+cd到unittest目录，然后直接命令行运行： go test
+如果想要显示详细的每个测试方法是否验证成功： go test -v
+*/
 func TestAdd(t *testing.T) {
 	if ans := Add(1, 2); ans != 3 {
 		t.Errorf("1 + 2 expected be 3, but %d got", ans)
