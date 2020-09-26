29733463fc6579ba1fcaa290669b6fd851da10ff
Jasper <1049578026@qq.com>
2020-09-26 21:27:04 +0800

    update

--- base/14_map.go
+++ base/14_map.go
@@ -17,21 +17,21 @@ func main() {
 	countryCapitalMap = make(map[string]string)
 
 	/* map插入key - value对,各个国家对应的首都 */
-	countryCapitalMap [ "France" ] = "巴黎"
-	countryCapitalMap [ "Italy" ] = "罗马"
-	countryCapitalMap [ "Japan" ] = "东京"
-	countryCapitalMap [ "India " ] = "新德里"
+	countryCapitalMap["France"] = "巴黎"
+	countryCapitalMap["Italy"] = "罗马"
+	countryCapitalMap["Japan"] = "东京"
+	countryCapitalMap["India "] = "新德里"
 
 	/*使用键输出地图值 */
 	for country := range countryCapitalMap {
-		fmt.Println(country, "首都是", countryCapitalMap [country])
+		fmt.Println(country, "首都是", countryCapitalMap[country])
 	}
 
 	/*查看元素在集合中是否存在 */
-	capital, ok := countryCapitalMap [ "American" ] /*如果确定是真实的,则存在,否则不存在 */
+	capital, ok := countryCapitalMap["American"] /*如果确定是真实的,则存在,否则不存在 */
 	/*fmt.Println(capital) */
 	/*fmt.Println(ok) */
-	if (ok) {
+	if ok {
 		fmt.Println("American 的首都是", capital)
 	} else {
 		fmt.Println("American 的首都不存在")
@@ -48,16 +48,22 @@ func main() {
 
 	/* 打印地图 */
 	for country := range countryCapitalMap2 {
-		fmt.Println(country, "首都是", countryCapitalMap2 [ country ])
+		fmt.Println(country, "首都是", countryCapitalMap2[country])
 	}
 
-	/*删除元素*/ delete(countryCapitalMap2, "France")
+	/*删除元素*/
+	delete(countryCapitalMap2, "France")
 	fmt.Println("法国条目被删除")
 
 	fmt.Println("删除元素后地图")
 
 	/*打印地图*/
 	for country := range countryCapitalMap2 {
-		fmt.Println(country, "首都是", countryCapitalMap2 [ country ])
+		fmt.Println(country, "首都是", countryCapitalMap2[country])
+	}
+
+	// key, value
+	for english, chinese := range countryCapitalMap2 {
+		fmt.Println("哈哈哈，key = ", english, ", value = ", chinese)
 	}
 }
